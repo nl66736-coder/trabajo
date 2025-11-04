f4e03d715532a71cabaa4243c8bb44d470188fe5
aleprog10 <alemorey05@gmail.com>
2025-11-04 13:47:47 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -20,4 +20,4 @@ class MenuNavegacion:
         menu.agregar_item("Comentarios", "#comentarios")
         return menu
 
-print("probando github online2,gjoighw")
+print("probando github online2,FASFASFAFSASF")
