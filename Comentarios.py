bec3a7306943c724d49a00ceb9c93f05a438e9d7
laurazuri14-wq <laurazuri14@gmail.com>
2025-11-19 19:38:09 +0100

    Update Comentarios.py

--- Comentarios.py
+++ Comentarios.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template_string, request, redirect
 from pagina_principal import PaginaPrincipal
+from render_html import RenderHTML
 
 app = Flask(__name__)
 pagina = PaginaPrincipal()
@@ -8,20 +9,9 @@ pagina.construir()
 @app.route('/')
 def index():
     html = pagina.render_html()
-    html = html.replace("</body>", """
-        <section id="nuevo-comentario" style="margin:20px;">
-            <h2>Deja tu comentario</h2>
-            <form action="/comentar" method="post">
-                <input type="text" name="autor" placeholder="Tu nombre" required><br><br>
-                <textarea name="texto" placeholder="Escribe tu comentario..." required></textarea><br><br>
-                <label>Valoración (1-5):</label>
-                <input type="number" name="valoracion" min="1" max="5" required><br><br>
-                <button type="submit">Enviar</button>
-            </form>
-        </section>
-    </body>""")
+    formulario = RenderHTML.render_formulario_nuevo_comentario()
+    html = html.replace("</body>", formulario + "</body>")
     return render_template_string(html)
-
 
 @app.route('/comentar', methods=['POST'])
 def comentar():
@@ -31,10 +21,16 @@ def comentar():
     pagina.seccion_comentarios.agregar_comentario(autor, texto, valoracion)
     return redirect('/')
 
-
 @app.route('/eliminar/<int:comentario_id>', methods=['POST'])
 def eliminar(comentario_id):
     pagina.seccion_comentarios.eliminar_comentario(comentario_id)
+    return redirect('/')
+
+@app.route('/editar/<int:comentario_id>', methods=['POST'])
+def editar(comentario_id):
+    nuevo_texto = request.form['texto']
+    nueva_valoracion = int(request.form['valoracion'])
+    pagina.seccion_comentarios.editar_comentario(comentario_id, nuevo_texto, nueva_valoracion)
     return redirect('/')
 
 
