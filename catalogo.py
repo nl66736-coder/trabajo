b6f74a53a1c1518e110836f71b18b7c356dc584d
aleprog10 <alemorey05@gmail.com>
2026-01-18 23:55:33 +0100

    Remove unused HTML rendering code

--- catalogo.py
+++ catalogo.py
@@ -5,8 +5,6 @@ import json
 import os
 from datetime import datetime
 
-html = RenderHTML.render_menu()
-html += "<section id='catalogo'><h1>Catálogo de productos</h1>"
 
 class CatalogoProductos:
     def __init__(self, archivo="catalogo.json"):
