dab1a20afd269f352febf11f208b97215144d120
aleprog10 <alemorey05@gmail.com>
2026-01-18 23:28:02 +0100

    Integrate HTML menu and shopping cart section
    Add HTML rendering for shopping cart section

--- carrito.py
+++ carrito.py
@@ -1,6 +1,8 @@
 from catalogo import CatalogoProductos
 
 catalogo = CatalogoProductos()
+html = RenderHTML.render_menu()
+html += "<section id='carrito'><h1>Carrito de Compras</h1>"
 
 class Carrito:
     def __init__(self):
